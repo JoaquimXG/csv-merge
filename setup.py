8b818acbd4844222a96f780beee55af278757ac1
Joaquim <49433786+JoaquimXG@users.noreply.github.com>
2022-02-09 16:34:49 +0000

    Improving README

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read()
 
 setuptools.setup(
-    name="merge-csv-JoaquimGomez",
-    version="0.0.7",
+    name="merge_csv_xjg",
+    version="0.0.2",
     author="Joaquim Gomez",
-    description="A small example package",
+    description="Merge two CSV files with pandas",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JoaquimXG/csv-merge",
@@ -19,6 +17,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     entry_points={ 'console_scripts': ['merge_csv = merge_csv.__main__:main' ] },
+    install_requires=['pandas', 'numpy', 'click'],
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
 )
